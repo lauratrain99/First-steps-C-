9f99bad4576160347505f5cb8ff598c3227e5d28
lauratrain99 <60850007+lauratrain99@users.noreply.github.com>
2020-06-17 18:44:08 +0200

    Add files via upload

---
+++ Overview/initials.cpp
@@ -0,0 +1,14 @@
+/* This program displays my initials */
+
+#include <iostream>
+int main() {
+
+  std::cout << "L      TTTTT\n";
+  std::cout << "L        T\n";
+  std::cout << "L        T\n";
+  std::cout << "L        T\n";
+  std::cout << "L        T\n";
+  std::cout << "L        T\n";
+  std::cout << "LLLLLL   T\n";
+
+}
