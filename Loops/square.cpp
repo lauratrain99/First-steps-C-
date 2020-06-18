c4c3af7a3e0be4037cab2e6893988219162fb85c
lauratrain99 <60850007+lauratrain99@users.noreply.github.com>
2020-06-18 11:35:29 +0200

    Add files via upload

---
+++ Loops/square.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  while (i < 10){
+    std::cout << i << "   " << square << "\n";
+    i++;
+    square = pow(i,2);
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+}
---
+++ Loops/steps.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  
+  // Output from 1 to steps:
+  
+  int steps = 10;
+  
+  for (int i = 1; i <= steps; i++) {
+    
+    std::cout << "Step #";
+    std::cout << i << "\n";
+    
+  }
+  
+}
