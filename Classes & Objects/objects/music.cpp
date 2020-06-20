728a61c26e5bc79c7a4278eeee2650e11d4a11d5
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2020-06-20 17:51:03 +0200

    Add files via upload

---
+++ Classes & Objects/objects/music.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "song.hpp"
+
+int main() {
+  Song electric_relaxation;
+  electric_relaxation.add_title("Electric Relaxation");
+  std::cout << electric_relaxation.get_title();
+  
+  
+}
