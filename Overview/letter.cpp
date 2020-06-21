1c63404eab63cc84a9254dd9e668f3b161e64ee3
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2020-06-21 17:09:38 +0200

    Add files via upload

---
+++ Overview/letter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "I want to become an aerospace engineer.\n";
+  std::cout << "My name is Laura and today is June 17th.";
+  
+  
+  
+  
+  
+}
